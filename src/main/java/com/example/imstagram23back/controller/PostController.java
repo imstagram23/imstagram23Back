3438c018050bb1f36d3b9e48c96059c1c3abc953
mangdo <70243735+mangdo@users.noreply.github.com>
2021-07-17 08:17:02 +0900

    Post 조회, 작성, 삭제 기능 구현

--- src/main/java/com/example/imstagram23back/controller/PostController.java
+++ src/main/java/com/example/imstagram23back/controller/PostController.java
@@ -1,4 +1,50 @@
 package com.example.imstagram23back.controller;
 
+import com.example.imstagram23back.domain.dto.PostRequestDto;
+import com.example.imstagram23back.domain.dto.PostResponseDto;
+import com.example.imstagram23back.service.PostService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/*
+* 2021-07-16 20:40 by 최민서
+*/
+
+@RequestMapping("/api/post")
+@RequiredArgsConstructor
+@RestController
 public class PostController {
+
+    private final PostService postService;
+
+    // Post 목록 최신순 조회
+    @GetMapping("")
+    public List<PostResponseDto> getPostList(){
+        return postService.getPostList();
+    }
+
+    // Post 생성
+    @PostMapping("")
+    public Map<String, Long> save( @RequestBody PostRequestDto requestDto){
+        String writer = "르탄이";
+        String imageUrl = "http://sssssss";
+
+        Map<String, Long> map= new HashMap<>();
+        map.put("postId", postService.save(requestDto, writer, imageUrl));
+        return map;
+    }
+
+    // Post 삭제
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id){
+        postService.delete(id);
+    }
+
+
 }
--- src/main/java/com/example/imstagram23back/service/PostService.java
+++ src/main/java/com/example/imstagram23back/service/PostService.java
@@ -1,4 +1,50 @@
 package com.example.imstagram23back.service;
 
+import com.example.imstagram23back.domain.dto.PostRequestDto;
+import com.example.imstagram23back.domain.dto.PostResponseDto;
+import com.example.imstagram23back.domain.model.Post;
+import com.example.imstagram23back.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/*
+ * 2021-07-16 20:40 by 최민서
+ */
+
+@RequiredArgsConstructor
+@Service
 public class PostService {
+
+    private final PostRepository postRepository;
+
+    // Post 목록 최신순 조회
+    @Transactional
+    public List<PostResponseDto> getPostList(){
+
+        return postRepository.findAllByOrderByCreatedAtDesc().stream()
+                .map(post -> new PostResponseDto(post))
+                .collect(Collectors.toList());
+    }
+
+    // Post 작성
+    @Transactional
+    public Long save(PostRequestDto requestDto, String writer, String imageUrl){
+        Post post = new Post(requestDto, writer, imageUrl);
+        return postRepository.save(post).getPostId();
+    }
+
+    // Post 삭제
+    @Transactional
+    public void delete(Long id){
+        try {
+            postRepository.deleteById(id);
+        }catch (EmptyResultDataAccessException e){
+                throw new IllegalArgumentException("해당 게시물이 없습니다. id = "+id);
+        }
+    }
 }
