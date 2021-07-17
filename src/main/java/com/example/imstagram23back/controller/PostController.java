33f803609293bd573298a1a10ba649dd3d382427
mangdo <70243735+mangdo@users.noreply.github.com>
2021-07-17 20:36:54 +0900

    의존성 수정, 필요없는 import 제외

--- src/main/java/com/example/imstagram23back/controller/PostController.java
+++ src/main/java/com/example/imstagram23back/controller/PostController.java
@@ -3,7 +3,6 @@ package com.example.imstagram23back.controller;
 import com.example.imstagram23back.domain.dto.PostRequestDto;
 import com.example.imstagram23back.domain.dto.PostResponseDto;
 import com.example.imstagram23back.service.PostService;
-import com.example.imstagram23back.util.S3Uploader;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -22,7 +21,6 @@ import java.util.Map;
 public class PostController {
 
     private final PostService postService;
-    private final S3Uploader s3Uploader;
 
     // Post 목록 최신순 조회
     @GetMapping("")
--- src/main/java/com/example/imstagram23back/controller/UserController.java
+++ src/main/java/com/example/imstagram23back/controller/UserController.java
@@ -7,13 +7,10 @@ import com.example.imstagram23back.domain.dto.TokenDto;
 import com.example.imstagram23back.domain.dto.TokenRequestDto;
 import com.example.imstagram23back.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
 
 
 @RequiredArgsConstructor
